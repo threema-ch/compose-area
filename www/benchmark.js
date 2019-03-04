bd52fd4b034e5cdff7597078a9512c12b8ff3f31
Danilo Bargen <danilo.bargen@threema.ch>
2019-03-04 12:19:02 +0100

    Set up benchmarking bundle

---
+++ www/benchmark.js
@@ -0,0 +1,8 @@
+import * as wasm from 'compose-area';
+
+// Assign wasm module to window object for testing purposes.
+window.wasm = wasm;
+
+document.getElementById('start').addEventListener('click', (e) => {
+    alert('Not yet implemented');
+});
--- www/webpack.common.js
+++ www/webpack.common.js
@@ -2,12 +2,16 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-    entry: './bootstrap.js',
+    entry: {
+        demo: './bootstrap.demo.js',
+        benchmark: './bootstrap.benchmark.js',
+    },
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'bootstrap.js',
+        filename: 'bootstrap.[name].bundle.js',
     },
     plugins: [
-        new CopyWebpackPlugin(['index.html'])
+        new CopyWebpackPlugin(['index.html']),
+        new CopyWebpackPlugin(['benchmark.html']),
     ],
 };
