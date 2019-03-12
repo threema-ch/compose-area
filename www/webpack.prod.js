48ae954f4689de54235329d9cb89b4fcf937da7d
Danilo Bargen <danilo.bargen@threema.ch>
2019-03-12 16:59:07 +0100

    Fix benchmarks in production
    The prod WebPack config would break the benchmarks due to minification.

--- www/webpack.prod.js
+++ www/webpack.prod.js
@@ -1,6 +1,39 @@
+const webpack = require('webpack');
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 
+
 module.exports = merge(common, {
-    mode: 'production',
+    mode: 'none',
+    devtool: 'source-map',
+    performance: {
+        hints: 'warning'
+    },
+    output: {
+        pathinfo: false
+    },
+    optimization: {
+        namedModules: false,
+        namedChunks: false,
+        nodeEnv: 'production',
+        flagIncludedChunks: true,
+        occurrenceOrder: true,
+        sideEffects: true,
+        usedExports: true,
+        concatenateModules: true,
+        splitChunks: {
+            hidePathInfo: true,
+            minSize: 30000,
+            maxAsyncRequests: 5,
+            maxInitialRequests: 3,
+        },
+        noEmitOnErrors: true,
+        checkWasmTypes: true,
+        minimize: false,
+    },
+    plugins: [
+        new webpack.DefinePlugin({ "process.env.NODE_ENV": JSON.stringify("production") }),
+        new webpack.optimize.ModuleConcatenationPlugin(),
+        new webpack.NoEmitOnErrorsPlugin()
+    ]
 });
