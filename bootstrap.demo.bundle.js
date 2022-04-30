36da8df376ced141a53fdfa2f442e0d549d42e42
ci-build <ci-build@circleci>
2022-04-30 17:54:28 +0000

    [skip ci] Updates

--- bootstrap.demo.bundle.js
+++ bootstrap.demo.bundle.js
@@ -670,7 +670,7 @@ __webpack_require__.e(/* import() */ 4).then(__webpack_require__.bind(__webpack_
 /******/ 					promises.push(installedWasmModuleData);
 /******/ 				else {
 /******/ 					var importObject = wasmImportObjects[wasmModuleId]();
-/******/ 					var req = fetch(__webpack_require__.p + "" + {"4":{"6":"6c029e7db367acdeb288"}}[chunkId][wasmModuleId] + ".module.wasm");
+/******/ 					var req = fetch(__webpack_require__.p + "" + {"4":{"6":"8f1b573007d2a5a5f91b"}}[chunkId][wasmModuleId] + ".module.wasm");
 /******/ 					var promise;
 /******/ 					if(importObject && typeof importObject.then === 'function' && typeof WebAssembly.compileStreaming === 'function') {
 /******/ 						promise = Promise.all([WebAssembly.compileStreaming(req), importObject]).then(function(items) {
